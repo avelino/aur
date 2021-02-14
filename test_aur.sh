91d8491f72a0abb5acad0c44312671cc58234f2d
Avelino <t@avelino.xxx>
2021-02-13 22:14:42 -0300

    install bash
    Signed-off-by: Avelino <t@avelino.xxx>

--- test_aur.sh
+++ test_aur.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pacman --noconfirm -Ss curl which base
+pacman --noconfirm -Ss curl which base bash
 source <(curl -s https://raw.githubusercontent.com/torokmark/assert.sh/master/assert.sh)
 
 PACKAGE_TEST_NAME="mosh"
